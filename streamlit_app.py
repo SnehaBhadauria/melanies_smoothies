af43905047c8c2a0f3cdd13ac54f17c32a117dda
Sneha Bhadauria <61448519+SnehaBhadauria@users.noreply.github.com>
2024-04-22 13:13:17 +0530

    Update streamlit_app.py
    removed the requests API from the if block

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 ## The addition of request for the api call
 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
