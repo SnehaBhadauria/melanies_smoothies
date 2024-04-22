43858d8f7c66c4c78267d175ddcf9739316196d7
Sneha Bhadauria <61448519+SnehaBhadauria@users.noreply.github.com>
2024-04-22 13:07:09 +0530

    Update streamlit_app.py
    Request API updation

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,15 @@ if ingredients_list:
     # st.write(my_insert_stmt)
     # st.stop()
     
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
-
+   
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
+## The addition of request for the api call
+
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st.text(fruityvice_response)
+
